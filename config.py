dbb7a2d9e3ffb80af675a05967cd1da92f0da4e0
learner337 <134784794+learner337@users.noreply.github.com>
2023-06-02 14:15:24 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+api_key = "EztE3Nynzk6jzFkTyeJtp2TlO"
+api_key_secret = "hOdhdFmmF4udHtHLjLNumfrJII2RTAAN1d75nUsCq7eWbrDs4I"
+access_token = "1505181061692723203-2Pr6TRIZv7yupPYnuUiwYdi62mXaAP"
+access_token_secret = "XdkCdvkpCBWPZgxh80f1H6mOzluh1PuNvGW79eDPhIgwt"
+
+all_user_names = ['BillGates', 'elonmusk', 'tim_cook']
+twitter_data_csv_file_name = "twitterScrape.csv"
+
+
+food_delivery_apps = ['zomato','Swiggy','DunzoIt','faasos', 'letsblinkit']
+csv_of_food_delivery_apps = 'foodDeliveryApps.csv'
+csv_of_food_delivery_apps_latest = 'foodDeliveryAppsLatest.csv'
+
+top_restaurants_names_in_hyderabad = ['Paradise','Bahar','Chutneys','Green Bawarchi','Mehfil','Pista House','Shah Gouse','Almond House','Creamstone', 'Cafe Coffee Day']
+top_restaurants_names_in_bangalore = ['The Humming Tree','Farzi Cafe', 'KFC', 'Pizza Hut','Dominos', 'Absolute Barbecue', 'The Coffee Brewery']
+csv_of_tweets_using_keywords = 'tweetsOfKeyWords.csv'
+
+top_restaurants_usernames_in_hyderabad = ['cafebaharhyd', 'pistahousehyd', 'CafeCoffeeDay', 'MehfilBiryani', 'Almondsforyou' ,'Creamstone']
+csv_of_top_restaurants_usernames_in_hyderabad = 'tweetsOfHyderabadRestaurants.csv'
+csv_of_top_restaurants_usernames_in_hyderabad_latest = 'tweetsOfHyderabadRestaurantsLatest.csv'
+
+top_restaurants_usernames_in_bangalore = ['FarziCafe', 'KFC_India', 'PizzaHutIN','dominos_india', 'absolutebbq', 'CoffeeBrewery']
+csv_of_top_restaurants_usernames_in_bangalore = 'tweetsOfBangaloreRestaurants.csv'
+csv_of_top_restaurants_usernames_in_bangalore_latest = 'tweetsOfBangaloreRestaurantsLatest.csv'
+
+csv_of_merged_food = 'csvOfMergedFood.csv'
+
+csv_of_sentiment_analysis = 'sentimentAnalysis.csv'
+
